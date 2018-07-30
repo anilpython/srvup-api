7fb96e6c90a9d0da486e2d2c3a3382a745e096b1
Anil <anilpython1@gmail.com>
2018-07-31 01:07:27 +0530

    Committing whole project

---
+++ src/videos/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework import routers, serializers, viewsets, permissions
+
+from .models import Video
+
+class VideoSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Video
+		fields = [
+		'id',
+		'slug',
+		'title',
+		'order',
+		'embed_code',
+		'share_message',
+		'timestamp',
+		]
+
+class VideoViewSet(viewsets.ModelViewSet):
+	authentication_classes = [SessionAuthentication, BasicAuthentication]
+	permission_classes = [permissions.IsAuthenticated]
+	queryset = Video.objects.all()
+	serializer_class = VideoSerializer
